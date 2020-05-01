d945a6929def2fc793caeef465ab35ac4d13b3f9
Alex <alexcfurtuna@gmail.com>
2020-05-01 22:04:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//smooth scroll
+$(document).ready(function() {
+    $("a[href*='#']:not([href='#])").click(function() {
+      let target = $(this).attr("href");
+      $('html,body').stop().animate({
+        scrollTop: $(target).offset().top
+      }, 1000);
+      event.preventDefault();
+    });
+  });
+
+  // nav //
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+  //toggle nab
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      //animate link
+       navLinks.forEach((link, index) => {
+         if (link.style.animation) {
+           link.style.animation = '';
+         } else {
+           link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+         }
+       });
+       //burger animation
+       burger.classList.toggle('toggle');
+     });
+   }
+  navSlide();
+
+  $(window).on("load", function() {
+	$('.masonry').masonry({
+    itemSelector: '.masonry-item'
+  });
+});
+  
